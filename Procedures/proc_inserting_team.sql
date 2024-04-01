c0b607c8293c7c2355ab0ddc785233670e6386b8
narintahmaz <136226603+narintahmaz@users.noreply.github.com>
2024-04-01 16:48:40 +0400

    proc_inserting_team.sql

--- Procedures/proc_inserting_team.sql
+++ Procedures/proc_inserting_team.sql
@@ -4,9 +4,9 @@ CREATE OR REPLACE PROCEDURE populate_team(
     p_employee_names IN string_array
 )
 AS
-    team_seq_val NUMBER; -- Declare a variable to hold the sequence value
+    team_seq_val NUMBER;
 BEGIN
-    -- Get the next value from the sequence
+   
     SELECT team_seq.NEXTVAL INTO team_seq_val FROM DUAL;
 
     IF p_role_names.COUNT <> p_employee_names.COUNT THEN
@@ -30,25 +30,12 @@ BEGIN
     populate_team(p_project_id => 123, p_role_names => roles, p_employee_names => employees);
 END;
 /
-Select * from project_team
-
-delete project_team
 
 Create table project_team
 (team_id number,
 role_name varchar2(50),
 employee_name varchar2(50),
-project_id number
---CONSTRAINT fk_supplier_comp FOREIGN KEY (project_id)REFERENCES projects (project_id),
----CONSTRAINT fk_supplier_comp FOREIGN KEY (role_name)REFERENCES roles (role_name),
---CONSTRAINT fk_supplier_comp FOREIGN KEY (employee_name)REFERENCES employee(employee_name)
-)
+project_id number )
 
-select * from projects
 
-select * from roles
-
-select * from employee
-
-drop table project_team
 
